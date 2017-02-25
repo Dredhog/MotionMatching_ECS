e93f3be3e99180196afdae81784e7343224fd61f
Rytis Pranckūnas <rytis.pranckunas@gmail.com>
2017-02-25 23:15:59 +0200

    Draw a triangle.

--- main.cpp
+++ main.cpp
@@ -57,6 +57,79 @@ Init(SDL_Window** Window)
   return Success;
 }
 
+void
+setupDrawTriangle(GLuint *VAO, GLuint *shaderProgram)
+{
+  // Triangle vertices
+  GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+  };
+
+  //Vertex shader source code
+  const GLchar *vertexShaderSource = "#version 330 core\n\nlayout (location = 0) in vec3 position;\n\nvoid main()\n{\n  gl_Position = vec4(position.x, position.y, position.z, 1.0);\n}";
+
+  //Fragment shader source code
+  const GLchar *fragmentShaderSource = "#version 330 core\n\nout vec4 color;\n\nvoid main()\n{\n  color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n}";
+
+
+  //Setting up vertex array object
+  glGenVertexArrays(1, VAO);
+  glBindVertexArray(*VAO);
+
+  //Setting up vertex buffer object
+  GLuint VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  //Setting up vertex shader
+  GLuint vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  
+  //Checking if vertex shader compiled successfully
+  GLint success;
+  GLchar infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    printf("Shader compilation failed!\n");
+  }
+
+  //Setting up fragment shader
+  GLuint fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  
+  //Checking if fragment shader compiled successfully
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    printf("Shader compilation failed!\n");
+  }
+
+  //Setting up shader program
+  *shaderProgram = glCreateProgram();
+  glAttachShader(*shaderProgram, vertexShader);
+  glAttachShader(*shaderProgram, fragmentShader);
+  glLinkProgram(*shaderProgram);
+
+  //Deleting linked shaders
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
+}
+
 int
 main(int argc, char* argv[])
 {
@@ -69,6 +142,11 @@ main(int argc, char* argv[])
   glViewport(0, 0, 640, 480);
 
   SDL_Event Event;
+
+  GLuint VAO;
+  GLuint shaderProgram;
+  setupDrawTriangle(&VAO, &shaderProgram);
+  
   // Main loop
   while (true)
   {
@@ -81,9 +159,14 @@ main(int argc, char* argv[])
     {
       break;
     }
-
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    //Drawing triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
 
     SDL_GL_SwapWindow(Window);
     SDL_Delay(16);
