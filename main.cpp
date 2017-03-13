fadf15f9571c19608d739a465ece220c1b386f43
Rytis Pranckūnas <rytis.pranckunas@gmail.com>
2017-03-13 17:32:28 +0200

    Add audio handle.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,63 @@
 #include "linear_math/vector.h"
 #include "linear_math/matrix.h"
 
+struct sdl_audio_ring_buffer
+{
+  int   Size;
+  int   WriteCursor;
+  int   PlayCursor;
+  void* Data;
+};
+
+static sdl_audio_ring_buffer AudioRingBuffer;
+
+void
+SDLAudioCallback(void* UserData, uint8_t* AudioData, int Length)
+{
+  assert(UserData);
+  assert(AudioData);
+  sdl_audio_ring_buffer* RingBuffer = (sdl_audio_ring_buffer*)UserData;
+
+  int Region1Size = Length;
+  int Region2Size = 0;
+  if(RingBuffer->PlayCursor + Length > RingBuffer->Size)
+  {
+    Region1Size = RingBuffer->Size - RingBuffer->PlayCursor;
+    Region2Size = Length - Region1Size;
+  }
+  memcpy(AudioData, (uint8_t*)(RingBuffer->Data) + RingBuffer->PlayCursor, Region1Size);
+  memcpy(&AudioData[Region1Size], RingBuffer->Data, Region2Size);
+  RingBuffer->PlayCursor  = (RingBuffer->PlayCursor + Length) % RingBuffer->Size;
+  RingBuffer->WriteCursor = (RingBuffer->PlayCursor + 2048) % RingBuffer->Size;
+}
+
+bool
+InitAudio(SDL_AudioDeviceID* AudioDevice, int SamplesPerSecond, int BufferSize)
+{
+  AudioRingBuffer.Size        = BufferSize;
+  AudioRingBuffer.Data        = malloc(AudioRingBuffer.Size);
+  AudioRingBuffer.PlayCursor  = 0;
+  AudioRingBuffer.WriteCursor = 0;
+
+  SDL_AudioSpec AudioSettings = { 0 };
+
+  AudioSettings.freq     = SamplesPerSecond;
+  AudioSettings.format   = AUDIO_S16LSB; // Check actual format type.
+  AudioSettings.channels = 2;
+  AudioSettings.samples  = 1024;
+  AudioSettings.callback = &SDLAudioCallback;
+  AudioSettings.userdata = &AudioRingBuffer;
+
+  *AudioDevice = SDL_OpenAudioDevice(NULL, 0, &AudioSettings, 0, 0);
+
+  if(*AudioDevice == 0)
+  {
+    printf("Failed to get an audio device: %s\n", SDL_GetError());
+    return false;
+  }
+  return true;
+}
+
 bool
 Init(SDL_Window** Window)
 {
@@ -103,8 +160,25 @@ main(int argc, char* argv[])
     return -1;
   }
 
+  int      SamplesPerSecond     = 48000;
+  int      ToneHz               = 256;
+  int16_t  ToneVolume           = 10000;
+  uint32_t RunningSampleIndex   = 0;
+  int      SquareWavePeriod     = SamplesPerSecond / ToneHz;
+  int      HalfSquareWavePeriod = SquareWavePeriod / 2;
+  int      BytesPerSample       = sizeof(int16_t) * 2;
+  int      SecondaryBufferSize  = SamplesPerSecond * BytesPerSample;
+  bool     SoundIsPlaying       = true;
+
+  SDL_AudioDeviceID AudioDevice;
+  if(!InitAudio(&AudioDevice, SamplesPerSecond, SecondaryBufferSize))
+  {
+    return -1;
+  }
+  SDL_PauseAudioDevice(AudioDevice, 0);
+
   // Asset loading
-  Mesh::mesh Mesh = Mesh::LoadOBJMesh("./data/suzane.obj\0", false, true, false);
+  Mesh::mesh Mesh = Mesh::LoadOBJMesh("./data/armadillo.obj\0", false, true, false);
   if(!Mesh.VerticeCount)
   {
     printf("ReadOBJ error: no vertices read\n");
@@ -115,13 +189,13 @@ main(int argc, char* argv[])
     SetUpMesh(&Mesh);
   }
 
-  int ShaderVertexColor = LoadShader("./shaders/color");
+  int ShaderVertexColor = Shader::LoadShader("./shaders/color");
   if(ShaderVertexColor < 0)
   {
     printf("Shader loading failed!\n");
   }
 
-  int ShaderWireframe = LoadShader("./shaders/wireframe");
+  int ShaderWireframe = Shader::LoadShader("./shaders/wireframe");
   if(ShaderVertexColor < 0)
   {
     printf("Shader loading failed!\n");
@@ -137,6 +211,7 @@ main(int argc, char* argv[])
   glEnable(GL_CULL_FACE);
 
   float AngleDeg = 0.0f;
+
   // Main loop
   while(true)
   {
@@ -151,8 +226,62 @@ main(int argc, char* argv[])
       break;
     }
 
+    SDL_LockAudioDevice(AudioDevice);
+
+    int ByteToLock = RunningSampleIndex * BytesPerSample % SecondaryBufferSize;
+    int BytesToWrite;
+    if(ByteToLock > AudioRingBuffer.PlayCursor)
+    {
+      BytesToWrite = (SecondaryBufferSize - ByteToLock);
+      BytesToWrite += AudioRingBuffer.PlayCursor;
+    }
+    else
+    {
+      BytesToWrite = AudioRingBuffer.PlayCursor - ByteToLock;
+    }
+
+    void* Region1     = (uint8_t*)AudioRingBuffer.Data + ByteToLock;
+    int   Region1Size = BytesToWrite;
+    if(Region1Size + ByteToLock > SecondaryBufferSize)
+    {
+      Region1Size = SecondaryBufferSize - ByteToLock;
+    }
+    void* Region2     = AudioRingBuffer.Data;
+    int   Region2Size = BytesToWrite - Region1Size;
+
+    int16_t* SampleOut          = (int16_t*)Region1;
+    int      Region1SampleCount = Region1Size / BytesPerSample;
+
+    for(int i = 0; i < Region1SampleCount; i++)
+    {
+      int16_t SampleValue =
+        ((RunningSampleIndex++ / HalfSquareWavePeriod) % 2) ? ToneVolume : -ToneVolume;
+      *SampleOut++ = SampleValue;
+      *SampleOut++ = SampleValue;
+    }
+
+    SampleOut              = (int16_t*)Region2;
+    int Region2SampleCount = Region2Size / BytesPerSample;
+
+    for(int i = 0; i < Region2SampleCount; i++)
+    {
+      int16_t SampleValue =
+        ((RunningSampleIndex++ / HalfSquareWavePeriod) % 2) ? ToneVolume : -ToneVolume;
+      *SampleOut++ = SampleValue;
+      *SampleOut++ = SampleValue;
+    }
+
+    SDL_UnlockAudioDevice(AudioDevice);
+
+    if(!SoundIsPlaying)
+    {
+      SDL_PauseAudioDevice(AudioDevice, 0);
+      SoundIsPlaying = true;
+    }
+
     // Update
-    mat4 ModelMatrix   = Math::MulMat4(Math::Mat4RotateY(AngleDeg * 0.2f), Math::Mat4Scale(0.25f));
+    mat4 ModelMatrix =
+      Math::MulMat4(Math::Mat4RotateY(180 + AngleDeg * 0.2f), Math::Mat4Scale(0.25f));
     mat4 CameraMatrix  = Math::Mat4Camera({ 0, 0, 1 }, { 0, 0, -1 }, { 0, 1, 0 });
     mat4 ProjectMatrix = Math::Mat4Perspective(60.f, 1028 / 800, 0.1f, 100.0f);
     mat4 MVPMatrix     = Math::MulMat4(ProjectMatrix, Math::MulMat4(CameraMatrix, ModelMatrix));
@@ -173,7 +302,7 @@ main(int argc, char* argv[])
     glDrawElements(GL_TRIANGLES, Mesh.IndiceCount, GL_UNSIGNED_INT, 0);
 #endif
 
-#if 1
+#if 0
     // Switch to wireframe shader
     glUseProgram(ShaderWireframe);
     glUniformMatrix4fv(glGetUniformLocation(ShaderVertexColor, "mat_mvp"), 1, GL_FALSE,
@@ -190,6 +319,7 @@ main(int argc, char* argv[])
     AngleDeg++;
   }
 
+  SDL_CloseAudioDevice(AudioDevice);
   SDL_DestroyWindow(Window);
   SDL_Quit();
   return 0;
