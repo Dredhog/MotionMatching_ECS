fe86fed30c381a80408a34925010e6e1d48ff443
Rytis Pranckunas <rytis.pranckunas@gmail.com>
2017-06-28 13:34:16 +0300

    Move platform specific files to their respective folders. Change build and make files accordingly.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mkdir win32_build
-pushd win32_build
-cl -Zi /std:c++latest ..\*.cpp ..\linear_math\*.cpp /Fe: engine glew32.lib opengl32.lib SDL2main.lib SDL2.lib SDL2_image.lib SDL2_ttf.lib /link /SUBSYSTEM:CONSOLE
+mkdir msvs_build
+pushd msvs_build
+cl -Zi /std:c++latest /I ..\ ..\win32\*.cpp ..\*.cpp ..\linear_math\*.cpp /Fe: engine glew32.lib opengl32.lib SDL2main.lib SDL2.lib SDL2_image.lib SDL2_ttf.lib /link /SUBSYSTEM:CONSOLE
 popd
