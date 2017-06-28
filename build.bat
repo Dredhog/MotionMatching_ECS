42804cbd0467f6feda5df9928a9fee71f16e32b9
Rytis Pranckunas <rytis.pranckunas@gmail.com>
2017-06-28 13:57:56 +0300

    Change build.bat file to use locally stored include and lib files.

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 mkdir msvs_build
 pushd msvs_build
-cl -Zi /std:c++latest /I ..\ ..\win32\*.cpp ..\*.cpp ..\linear_math\*.cpp /Fe: engine glew32.lib opengl32.lib SDL2main.lib SDL2.lib SDL2_image.lib SDL2_ttf.lib /link /SUBSYSTEM:CONSOLE
+cl -Zi /std:c++latest /I ..\ /I ..\include ..\win32\*.cpp ..\*.cpp ..\linear_math\*.cpp /Fe: engine ..\lib\glew32.lib opengl32.lib ..\lib\SDL2main.lib ..\lib\SDL2.lib ..\lib\SDL2_image.lib ..\lib\SDL2_ttf.lib /link /SUBSYSTEM:CONSOLE
 popd
